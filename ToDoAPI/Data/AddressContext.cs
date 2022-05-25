26bf57575b919c049df2a636a720644d36f2d21f
Imharia Ughu <imhariaosoria@gmail.com>
2022-05-25 10:09:46 +0100

    Add project files.

---
+++ ToDoAPI/Data/AddressContext.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoAPI.Data
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options) : base(options)
+        {
+        }
+        public DbSet<AddressItem> AddressItems { get; set; }
+    }
+}
